0511393b763111c6663246ebe49e1e5fe4d60b41
Lauro Cesar <lauroandradearaujo@gmail.com>
2020-11-27 10:46:30 -0300

    Adiciona funções já prontas pro MongoDB

--- tech_news/database.py
+++ tech_news/database.py
@@ -14,3 +14,19 @@ def insert_or_update(notice):
 
 def check_duplicates(news):
     """Seu código deve vir aqui"""
+
+
+def create_news(data):
+    db.news.insert_many(data)
+
+
+def insert_or_update(notice):
+    return  db.news.update_one({"url": notice['url']}, {"$set": notice}, upsert=True).upserted_id is not None
+
+
+def find_news():
+    return list(db.news.find({}, {'_id': False}))
+
+
+def search_news(query):
+    return list(db.news.find(query))
